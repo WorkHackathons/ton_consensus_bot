79bea04ceb859f5ffae74cc0701d51c792a9da2c
luzzwaix <155660663+luzzwaix@users.noreply.github.com>
2026-03-22 20:10:45 +0600

    Delete miniapp-react directory

--- miniapp-react/src/constants.js
+++
@@ -1,8 +0,0 @@
-export const API = (import.meta.env.VITE_API_URL || window.location.origin).replace(/\/$/, "");
-export const BOT = "ton_consensus_bot";
-export const tg = window.Telegram?.WebApp;
-
-tg?.ready();
-tg?.expand();
-
-export const userId = tg?.initDataUnsafe?.user?.id;
